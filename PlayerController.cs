8283dbfbfb16a81d97fee496ac3b954f58441fd2
Stefan Pla <spla@icloud.com>
2025-01-29 17:25:57 -0500

    filled out playercontroller script with initial jump logic

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody playerRb;
+    public float jumpForce = 15;
+    public float gravityModifier = 3;
+    public bool isOnGround = true;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        isOnGround = true;
     }
 }
