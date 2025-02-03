6f79abb054d77b6f8116b95dad024a2f321bdbed
Stefan Pla <spla@icloud.com>
2025-02-03 15:52:45 -0500

    background/obstacles stop moving when player hits an obstacle. obstacles also delete when they get past a certain point on the screen.

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25,0,0);
     private float startDelay = 2.0f;
     private float repeatRate = 2.0f;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
